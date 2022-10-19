fdf08f1f18a2372a80a004fa61ef450d07c17510
Remix Workspace <>
2022-10-18 23:28:13 -0500

    I changed some things and added to the readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Project_3_V2
 
+#Background
+//This project is to make a voting system for an art club that is having a show and has room for one more artist. The club is letting the members vote on a few options to
+//pick artist they want to see at the art show. The voting is done through an online poll and the artist with the highest amount of votes will win the spot at 
+//the art show and have a chance for their art to be published and sold. 
 
-//This project is to make a voting system for uses such as an NFT project that would like feedback from their community members. Members can vote on many things such as giveaways,
-whether or not to start a DOW, etc. Building a voting application on a decentralized database (DApp) ensures that it is more secure and less likely to be manipulated by outside parties
-aswell as making sure that each voter is authenticated and the community is able to propose ideas.
+#Info.
+//This project was wriitten on Remix IDE on the language Solidity. The operating system it was written on was Windows.
+
+#Creator
+Thomas Burns - Email: burns235577@gmail.com
 
--- Voting_DApp.sol
+++ Voting_DApp.sol
@@ -3,35 +3,36 @@ pragma solidity >= 0.8.0<0.9.0;
 contract Poll 
 {
 
-    //code for Poll ideas from members
+    //code for Poll considerations from members
 
     // I am creating my own data types using struct
 
-    struct Idea 
+    struct Considerations 
     {
        
-    string names; // this is the name of each individual idea
-    uint ticketNum; // number of combined ideas 
+    string names; // this is the name of each individual consideration
+    uint256 ticketNum; // number of combined considerations 
     }
 
 
     // Voters struct determines if members can access the voting, counts the number of votes, and if the members chose vote
-    struct Voters
+    struct Members
     {
-        bool didVote; //this many people chose to vote
         bool voterAccess; //determines if members can access the voting
         uint256 voteTracker; //tracks the number of votes
+        uint256 didVote; //this many people chose to vote
+        uint256 votersLeft; //number of voters that have not participated
     }  
 
 
-    Idea[] public ideas;
+    Consideration[] public considerations;
 
-    mapping(address => Voters) public members;
+    mapping(address => Members) public voters;
 
     address public moderator;
 
-    // the idea names will only be stored temporarilly until the voting is over
-    constructor(string[] memory ideaName) 
+    // the names being considered will only be stored temporarily until the voting is over
+    constructor(string[] memory considerationName) 
     {
 
         // we will give the moderator the msg.sender role in order to allow them to have control over the contract
@@ -39,17 +40,59 @@ contract Poll
         moderator = msg.sender; 
 
         // the moderator will be counted as 1 vote so we add that here 
-        members[moderator].voteTracker = 1;
+        voters[moderator].voteTracker = 1;
 
-        // this for loop will add the idea names for the contract when it is launched
-        for(uint i=0; i<ideaName.length; i++)
+        // this for loop will add the considered names for the contract when it is launched
+        for(uint256 i=0; i<considerationName.length; i++)
         {
-            ideas.push(Idea({
-                names: ideaName[i],
-                // we start the vote count at zero in ordre to make sure ther is no unfair advantage
+            considerations.push(Considerations({
+                names: considerationName[i],
+                // we start the vote count at zero in ordre to make sure there is no unfair advantage
                 ticketNum: 0
 
             }));
         }
     }
+
+    function allowAccessToVote(address member) public 
+    {
+        require (msg.sender == moderator,
+        'You may vote once the moderator allows you access to the poll');
+
+        require(!voters[member].voterAccess,
+        'Only one vote per member');    
+
+        // the .voterAccess boolean is to allow access or not depending on if they had previosly voted and .didVote is a numeric variable required to equal 0 in order for
+        // the member to vote
+
+        require(voters[member].didVote == 0);
+        
+        voters[member]didVote = 1;
+    }
+     function vote (uint256 consideration) public
+        {
+            Member storage sender = voters[msg.sender];
+            require(!sender.voterAccess, 'You have already voted for the poll');
+            require(sender.didVote !=0, 'This poll has already been voted on and your access has been removed');
+            sender.voted = true;
+            sender.vote = consideration;
+
+            ideas[idea].ticketNumb = considerations[consideration].ticketNumb + sender.didVote;
+            ideas[idea].votersLeft = considerations[consideration].; //comeback to this part which is the total number of members that have not voted
+        }
+
+        function theSuperiorCArtist()public view return (uint256 theSuperiorArtist_) {  
+            uint256 highestVoteCount = 0;
+            for (uint256 i = 0, i < considerations.length; i ++){
+                if(considerations[i] > highestVoteCount){
+                    highestVoteCount = considerations[i].ticketNumb;
+                    theSuperiorArtist_ = i;
+                }
+            }
+        }
+
+        function superiorArtist() public view return (string superiorArtist_) {
+            considerations[superiorArtist]
+
+        }
 }
