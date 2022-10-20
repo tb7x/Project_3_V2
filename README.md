5d35c447a58d63ebbea92689dd75cfcc7327f971
Remix Workspace <>
2022-10-20 00:42:41 -0500

    update

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Project_3_V2
 
 #Background
-//This project is to make a voting system for an art club that is having a show and has room for one more artist. The club is letting the members vote on a few options to
-//pick artist they want to see at the art show. The voting is done through an online poll and the artist with the highest amount of votes will win the spot at 
-//the art show and have a chance for their art to be published and sold. 
+This project is to make a voting system for an art club that is having a show and has room for one more artist. The club is letting the members vote on a few options to
+pick artist they want to see at the art show. The voting is done through an online poll and the artist with the highest amount of votes will win the spot at 
+the art show and have a chance for their art to be published and sold. 
 
 #Info.
-//This project was wriitten on Remix IDE on the language Solidity. The operating system it was written on was Windows.
+This project was wriitten on Remix IDE on the language Solidity. The operating system it was written on was Windows.
 
 #Creator
 Thomas Burns - Email: burns235577@gmail.com
--- Voting_DApp.sol
+++ Voting_DApp.sol
@@ -1,4 +1,5 @@
-pragma solidity >= 0.8.0<0.9.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 contract Poll 
 {
@@ -11,7 +12,7 @@ contract Poll
     {
        
     string names; // this is the name of each individual consideration
-    uint256 ticketNum; // number of combined considerations 
+    uint256 ticketNum; // the total number of combined considerations 
     }
 
 
@@ -21,17 +22,17 @@ contract Poll
         bool voterAccess; //determines if members can access the voting
         uint256 voteTracker; //tracks the number of votes
         uint256 didVote; //this many people chose to vote
-        uint256 votersLeft; //number of voters that have not participated
+
     }  
 
 
-    Consideration[] public considerations;
+    Considerations[] public considerations;
 
     mapping(address => Members) public voters;
 
     address public moderator;
 
-    // the names being considered will only be stored temporarily until the voting is over
+    // the names being considered will only be stored temporarily until the voting is over because we are using memory and not storage
     constructor(string[] memory considerationName) 
     {
 
@@ -43,10 +44,11 @@ contract Poll
         voters[moderator].voteTracker = 1;
 
         // this for loop will add the considered names for the contract when it is launched
-        for(uint256 i=0; i<considerationName.length; i++)
+        for(uint256 c=0; c<considerationName.length; c++)
         {
-            considerations.push(Considerations({
-                names: considerationName[i],
+            considerations.push(Considerations
+            ({
+                names: considerationName[c],
                 // we start the vote count at zero in ordre to make sure there is no unfair advantage
                 ticketNum: 0
 
@@ -59,40 +61,39 @@ contract Poll
         require (msg.sender == moderator,
         'You may vote once the moderator allows you access to the poll');
 
-        require(!voters[member].voterAccess,
-        'Only one vote per member');    
-
-        // the .voterAccess boolean is to allow access or not depending on if they had previosly voted and .didVote is a numeric variable required to equal 0 in order for
-        // the member to vote
-
+        require(!voters[member].voterAccess,// the .voterAccess boolean is to allow access or not depending on if they had previosly voted and .didVote is
+        'Only one vote per member');        //  a numeric variable required to equal 0 in order for the member to vot
         require(voters[member].didVote == 0);
         
         voters[member]didVote = 1;
     }
-     function vote (uint256 consideration) public
+     function vote(uint256 consideration) public
         {
-            Member storage sender = voters[msg.sender];
+            Members storage sender c = voters[msg.sender];
             require(!sender.voterAccess, 'You have already voted for the poll');
             require(sender.didVote !=0, 'This poll has already been voted on and your access has been removed');
             sender.voted = true;
             sender.vote = consideration;
 
             ideas[idea].ticketNumb = considerations[consideration].ticketNumb + sender.didVote;
-            ideas[idea].votersLeft = considerations[consideration].; //comeback to this part which is the total number of members that have not voted
         }
 
-        function theSuperiorCArtist()public view return (uint256 theSuperiorArtist_) {  
+        function theSuperiorCArtist()public view returns (uint256 theSuperiorArtist_) 
+        {  
             uint256 highestVoteCount = 0;
-            for (uint256 i = 0, i < considerations.length; i ++){
-                if(considerations[i] > highestVoteCount){
-                    highestVoteCount = considerations[i].ticketNumb;
-                    theSuperiorArtist_ = i;
+            for (uint256 c = 0; c < considerations.length; c ++)
+            {
+                if(considerations[c].ticketNumb > highestVoteCount)
+                {
+                    highestVoteCount = considerations[c].ticketNumb;
+                    theSuperiorArtist_ = c;
                 }
             }
         }
 
-        function superiorArtist() public view return (string superiorArtist_) {
-            considerations[superiorArtist]
+        function superiorArtist() public view returns (string superiorArtist_) 
+        {
+            superiorArtist_ = considerations[superiorArtist()].name;
 
         }
 }
